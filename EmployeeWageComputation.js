6e2d4108ad8a2faa2ff11924a1cb677b49eec151
Ranbir7925 <honeyranbir7925@gmail.com>
2020-10-30 22:10:55 +0530

    ADDED:Code to show Daily wage of Employee

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,12 +1,25 @@
-const IS_PRESENT = 1;
+const IS_PRESENT = 1
+const WAGE_PER_HOUR = 20
 
 function checkEmployerrAttendence(){
     var attendaceStatus = Math.floor(Math.random() * 2);
+    var employeeWorkingHours
 
-    if(attendaceStatus == IS_PRESENT)
-    console.log("Employee is PRESENT..!!")
-    else
-    console.log("Employee is ABSENT...!!")
+    if(attendaceStatus == IS_PRESENT){
+        console.log("Employee is PRESENT..!!")
+        employeeWorkingHours = 8
+    }
+    else{
+        console.log("Employee is ABSENT...!!")
+        employeeWorkingHours = 0
+    }
+    return employeeWorkingHours
 }
 
-checkEmployerrAttendence();
+function calculateEmployeeWage(workHrs){
+    return WAGE_PER_HOUR*workHrs
+}
+
+var workingHours = checkEmployerrAttendence()
+var dailyWage = calculateEmployeeWage(workingHours)
+console.log(`Employee daily wage is ${dailyWage}`)
